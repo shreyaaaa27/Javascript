532305e67fc70030258b1d05f4a235542fe48adf
shreyaaaa27 <shreya1.mitblr2023@learner.manipal.edu>
2025-12-02 16:21:28 +0000

    comparisons

---
+++ basics/comparisons.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+//comparisons (>,<,>=,=<) and equality checks(==) work differently
+// comparisons convert null to a number ,treating it as 0
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+//compares dataTypes of comparators
+
+console.log("2"===2);
