3c31c9859c0470b3f51b581b7464b4ee9d47ec33
shreyaaaa27 <shreya1.mitblr2023@learner.manipal.edu>
2025-12-02 11:43:54 +0000

    conversion of datatypes

---
+++ basics/operationsConversion.js
@@ -0,0 +1,26 @@
+let score = true
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc"=>NaN
+//true => 1, false=>0
+
+let isLoggedIn="Shreya"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+//1=> true, 0=>true
+//""=>false
+//"non-empty string"=>true
+
+let someNumber =33
+let stringNumber= String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
