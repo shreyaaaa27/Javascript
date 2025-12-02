84418b3dd982163f9f0d1e3b4cc92c1b0a954983
shreyaaaa27 <shreya1.mitblr2023@learner.manipal.edu>
2025-12-02 16:04:36 +0000

    conversion of operations

--- basics/operationsConversion.js
+++ basics/operationsConversion.js
@@ -1,11 +1,11 @@
 let score = true
 
-console.log(typeof score)
-console.log(typeof(score))
+//console.log(typeof score)
+//console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 //"33"=> 33
 //"33abc"=>NaN
@@ -13,7 +13,7 @@ console.log(valueInNumber);
 
 let isLoggedIn="Shreya"
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
+//console.log(booleanisLoggedIn)
 
 //1=> true, 0=>true
 //""=>false
@@ -21,6 +21,29 @@ console.log(booleanisLoggedIn)
 
 let someNumber =33
 let stringNumber= String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
 
+//****************Operations***********//
+let value =3
+let negValue =-value
+//console.log(negValue)
+
+let str1 ="hello"
+let str2 =" shreya"
+let str3 = str1 + str2
+// console.log(str3)
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((3+4)*5%3);
+//to comment multiple lines, select and press ctrl+"/" and do the same for uncommenting as well
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
