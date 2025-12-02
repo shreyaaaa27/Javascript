c4553d4879065f0e79261f572b83d9357b935b92
shreyaaaa27 <shreya1.mitblr2023@learner.manipal.edu>
2025-12-02 04:11:50 +0000

    learning variables

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const AccountId= 14453
+let AccountEmail = "shreya@google.com"
+var AccountPassword ="12345"
+accountCity="Jaipur"
+let accountState ;
+
+//accountId= 2 //not allowed
+
+AccountEmail="hc.hc@gmail.com"
+AccountPassword="21212121"
+AccountCity="Bengaluru"
+
+console.log(AccountId)
+/* prefer not to use var because of issuein block scope and functional scope */
+console.table([AccountId,AccountEmail,AccountPassword,AccountCity,accountState])
